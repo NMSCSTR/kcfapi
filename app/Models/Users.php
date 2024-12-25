2ff92de1570e9e9673d6b124b9f019a4177e951a
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2024-12-25 19:41:50 +0800

    Creating model for users

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Users extends Model
 {
-    //
+    protected $table = 'users';
+    
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'kcfapicode',
+        'email',
+        'phone_number',
+        'role',
+        'password'
+    ];
 }
