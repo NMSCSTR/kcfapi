b0dd05bad265463fbd7d1a11af010ad123de7f22
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2024-12-25 20:46:42 +0800

    Creating Controller and making file folder

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,7 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        return view('Auth.login');
     }
 
     /**
@@ -20,7 +20,7 @@ class UsersController extends Controller
      */
     public function create()
     {
-        //
+        return view('Auth.login');
     }
 
     /**
